ed3c616ec65104a9d91905e0b083adf1225538a8
leandrojamir <leandrojamir@hotmail.com>
2023-01-15 05:45:54 -0300

    forçando novamente reenvio secao 3 req 28 para ver o que ou houve com req 23, teste local ok

--- app/backend/src/routes/matches.routes.ts
+++ app/backend/src/routes/matches.routes.ts
@@ -29,3 +29,30 @@ matchesRoutes.patch('/:id', MatchesControllers.updateMatchesControllers);
 
 export default matchesRoutes;
 // The self-hosted runner: self-hosted-runner-vdxk2-7mbzf lost communication with the server
+// jamir@jamir-X550CA:~/Projetos/back-end/sd-022-b-trybe-futebol-clube$ npm test 05_
+
+// > trybe-futebol-club@1.0.0 pretest
+// > (cd ./app/backend && /bin/sh tsc_eval.sh)
+
+// > trybe-futebol-club@1.0.0 test
+// > env $(cat ./app/backend/.env) jest -i --forceExit --verbose "05_"
+
+// cat: ./app/backend/.env: Arquivo ou diretório inexistente
+//  PASS  __tests__/E2E/05_insertMatches.test.js (210.806 s)
+//   23 - Desenvolva o endpoint /matches de modo que seja possível salvar uma partida com o status de inProgress como true no banco de dados
+//     ✓ Será validado que é possível salvar um jogo no banco de dados e ver o jogo na página de jogos (34573 ms)
+//   24 - Desenvolva o endpoint `/matches/:id/finish` de modo que seja possível alterar o status inProgress de uma partida para false no banco de dados
+//     ✓ Será validado que ao finalizar uma partida é alterado no banco de dados e na página (35466 ms)
+//   25 - Desenvolva o endpoint /matches de forma que não seja possível inserir uma partida com times iguais
+//     ✓ Será validado que não é possivel inserir uma partida com times iguais (33226 ms)
+//   26 - Desenvolva o endpoint /matches de forma que não seja possível inserir uma partida com time que não existe na tabela teams
+//     ✓ Será validado na API que não é possível inserir uma partida com time que não existe na tabela teams (29012 ms)
+//   27 - Desenvolva o endpoint /matches de forma que não seja possível inserir uma partida sem um token válido
+//     ✓ Será validado na API que não é possível inserir uma partida com um token inválido (40887 ms)
+
+// Test Suites: 1 passed, 1 total
+// Tests:       5 passed, 5 total
+// Snapshots:   0 total
+// Time:        211.857 s
+// Ran all test suites matching /05_/i.
+// Force exiting Jest: Have you considered using `--detectOpenHandles` to detect async operations that kept running after all tests finished?
